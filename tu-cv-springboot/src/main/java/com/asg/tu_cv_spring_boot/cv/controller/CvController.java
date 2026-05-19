c9bdd5b7f4b505c87f021790a0db0523097d5bd5
alonso1298 <alonsosagrerogranados@gmail.com>
2026-05-18 22:06:07 -0600

    add html

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,7 +1,13 @@
 package com.asg.tu_cv_spring_boot.cv.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // Permite mapear rutas web
 public class CvController {
+
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
 }
