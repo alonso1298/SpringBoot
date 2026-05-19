dfa4f8ac06221c1e6669ebea6ef486bc6f8d44c8
alonso1298 <alonsosagrerogranados@gmail.com>
2026-05-18 21:49:20 -0600

    add firt project

---
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/TuCvSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.asg.tu_cv_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuCvSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuCvSpringbootApplication.class, args);
+	}
+
+}
---
+++ tu-cv-springboot/src/test/java/com/asg/tu_cv_spring_boot/TuCvSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asg.tu_cv_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuCvSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
