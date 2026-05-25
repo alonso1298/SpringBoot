57f212f53c69d421f24bd3e559f3c5180fa61793
alonso1298 <alonsosagrerogranados@gmail.com>
2026-05-24 22:14:47 -0600

    add Model

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
@@ -2,6 +2,7 @@ package com.asg.tu_cv_spring_boot.cv.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,15 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/cv") //Creamos una ruta base
 public class CvController {
 
-    @GetMapping("/home")
-    public String index(){
+    @GetMapping({"/index", "/", ""})
+    // Model es una interfaz, contenedor de datos, pasa informacion
+    // de controlador a una vista
+    public String index(Model model){
+        model.addAttribute("name", "Gabriel"); // Es un mapa que maneja una key
         return "index";
-    }
-
-    // Indicamos 3 rutas con un get mapping
-    @GetMapping("/pep")
-    public String pep(){
-        return "pep";
     }
 
 }
