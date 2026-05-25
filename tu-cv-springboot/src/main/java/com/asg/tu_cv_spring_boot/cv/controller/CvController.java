310ea35759b3d2febff3c6991f9c35a16d0cf9fd
alonso1298 <alonsosagrerogranados@gmail.com>
2026-05-24 21:38:22 -0600

    add RequestMapping

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,19 +1,24 @@
 package com.asg.tu_cv_spring_boot.cv.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller // Permite mapear rutas web
+@RequestMapping("/cv") //Creamos una ruta base
 public class CvController {
 
-//    @GetMapping("/home")
-//    public String index(){
-//        return "index";
-//    }
+    @GetMapping("/home")
+    public String index(){
+        return "index";
+    }
 
     // Indicamos 3 rutas con un get mapping
-    @GetMapping({"/pep", "", "/home"})
+    @GetMapping("/pep")
     public String pep(){
         return "pep";
     }
+
 }
