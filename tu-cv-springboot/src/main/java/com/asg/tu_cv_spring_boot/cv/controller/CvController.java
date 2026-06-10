fa51386ece3cccb4cbec1d456ebfb2bbe29dfa58
alonso1298 <alonsosagrerogranados@gmail.com>
2026-06-09 20:53:36 -0600

    refactor

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,11 +1,9 @@
 package com.asg.tu_cv_spring_boot.cv.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller // Permite mapear rutas web
 @RequestMapping("/cv") //Creamos una ruta base
