f928dd13b4e03eb7b54f8bd621b8f28b9beb267b
alonso1298 <alonsosagrerogranados@gmail.com>
2026-06-16 20:13:34 -0600

    add atribute

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,5 +1,6 @@
 package com.asg.tu_cv_spring_boot.cv.controller;
 
+import com.asg.tu_cv_spring_boot.cv.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,9 @@ public class CvController {
     // Model es una interfaz, contenedor de datos, pasa informacion
     // de controlador a una vista
     public String index(Model model){
+        Person persona = new Person("Gabriel", "Sagrero", "Dev");
         model.addAttribute("name", "Gabriel"); // Es un mapa que maneja una key
+        model.addAttribute("persona", persona); // Es un mapa que maneja una key
         return "index";
     }
 
