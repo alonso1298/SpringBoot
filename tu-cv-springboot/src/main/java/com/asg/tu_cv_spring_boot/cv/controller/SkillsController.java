bc824635e939acc0adf0d6ce4810e4c7ac2fd5d7
alonso1298 <alonsosagrerogranados@gmail.com>
2026-06-16 21:22:19 -0600

    add each in thymeleaf

---
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -0,0 +1,17 @@
+package com.asg.tu_cv_spring_boot.cv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class SkillsController {
+    @GetMapping("/skills")
+    public String habilidades(Model model){
+        List<String> habilidades = List.of("Java", "Spring", "Angular", "HTML", "JS");
+        model.addAttribute("skills", habilidades);
+        return "skills";
+    }
+}
