d8c8bca0ebec9780881b2f6b525bfc31510884d9
alonso1298 <alonsosagrerogranados@gmail.com>
2026-07-11 13:26:06 -0600

    add form

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/SkillsController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -4,6 +4,8 @@ import com.asg.tu_cv_spring_boot.cv.model.Skill;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -29,7 +31,12 @@ public class SkillsController {
     }
     @GetMapping("/new")
     public  String showForm(Model model){
-        model.addAttribute("skills", skills);
+        model.addAttribute("skills", new Skill());
         return "add-skills";
+    }
+    @PostMapping("/add")
+    public  String addSkill( @ModelAttribute Skill skill){
+        skills.add(skill);
+        return "redirect:/skills";
     }
 }
