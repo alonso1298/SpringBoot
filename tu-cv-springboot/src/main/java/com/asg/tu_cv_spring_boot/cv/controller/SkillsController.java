9b82f9d1f0025579b812495dbade2067d8072ae1
alonso1298 <alonsosagrerogranados@gmail.com>
2026-07-11 12:31:30 -0600

    add Model Atribute

--- tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/SkillsController.java
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -1,19 +1,35 @@
 package com.asg.tu_cv_spring_boot.cv.controller;
 
+import com.asg.tu_cv_spring_boot.cv.model.Skill;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/skills")
 public class SkillsController {
-    @GetMapping("/skills")
+
+    private final List<Skill> skills = new ArrayList<>();
+
+    @GetMapping
     public String habilidades(Model model){
         List<String> habilidades = List.of("Java", "Spring", "Angular", "HTML", "JS");
         List<String> list = new ArrayList<>();
         model.addAttribute("skills", list);
         return "skills";
+    }
+    @GetMapping
+    public  String showSkills(Model model){
+        model.addAttribute("skills", skills);
+        return "add-skills";
+    }
+    @GetMapping("/new")
+    public  String showForm(Model model){
+        model.addAttribute("skills", skills);
+        return "add-skills";
     }
 }
---
+++ tu-cv-springboot/src/main/java/com/asg/tu_cv_spring_boot/cv/model/Skill.java
@@ -0,0 +1,13 @@
+package com.asg.tu_cv_spring_boot.cv.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skill {
+    private String nombre;
+
+}
