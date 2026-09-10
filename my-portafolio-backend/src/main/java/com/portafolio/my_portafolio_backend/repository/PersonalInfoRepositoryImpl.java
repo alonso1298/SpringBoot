76d8e60677f79ba8a21ad62fc5c3d57566343aae
alonso1298 <alonsosagrerogranados@gmail.com>
2026-09-09 22:01:41 -0600

    add rowMapper

--- my-portafolio-backend/src/main/java/com/portafolio/my_portafolio_backend/repository/PersonalInfoRepositoryImpl.java
+++ my-portafolio-backend/src/main/java/com/portafolio/my_portafolio_backend/repository/PersonalInfoRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.portafolio.my_portafolio_backend.repository;
 
 import com.portafolio.my_portafolio_backend.model.PersonalInfo;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,28 @@ import java.util.Optional;
 @Repository
 public class PersonalInfoRepositoryImpl implements IPersonalInfoRepository{
 
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<PersonalInfo> personalInfoRowMapper = (rs, numRow) -> {
+        PersonalInfo info = new PersonalInfo();
+        info.setId(rs.getLong("id"));
+        info.setFirstName(rs.getString("first_name"));
+        info.setLastName(rs.getString("last_name"));
+        info.setTitle(rs.getString("title"));
+        info.setProfileDescription(rs.getString("profile_description"));
+        info.setProfileImageUrl(rs.getString("profile_image_url"));
+        info.setYearsOfExperience(rs.getObject("year_of_experience", Integer.class)); // Usar getObject para nulos
+        info.setEmail(rs.getString("email"));
+        info.setPhone(rs.getString("phone"));
+        info.setLikedinUrl(rs.getString("linkedin_url"));
+        info.setGithubUrl(rs.getString("github_url"));
+        return info;
+    };
+
+    public PersonalInfoRepositoryImpl(){
+        this.jdbcTemplate = new JdbcTemplate();
+    }
+
     @Override
     public PersonalInfo save(PersonalInfo personalInfo) {
         return null;
@@ -16,12 +40,14 @@ public class PersonalInfoRepositoryImpl implements IPersonalInfoRepository{
 
     @Override
     public Optional<PersonalInfo> findById(Long id) {
+        String sql = "";
         return Optional.empty();
     }
 
     @Override
     public List<PersonalInfo> findAll() {
-        return List.of();
+        String sql = "SELECT * FROM personal_info";
+        return jdbcTemplate.query(sql, personalInfoRowMapper);
     }
 
     @Override
